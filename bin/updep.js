8cab294ae9a3088b1b1268ffeff1d14b738f98f3
Michael Rodrigues <mrodrig@users.noreply.github.com>
2024-07-12 23:07:34 -0400

    July24 audit fix (#24)
    * chore(deps): npm audit fix
    
    * chore(deps): manual upgrades
    
    * fixup
    
    * chore(rel): 2.2.11

--- bin/updep.js
+++ bin/updep.js
@@ -2,47 +2,51 @@
 
 'use strict';
 
-let program = require('commander'),
+let { program } = require('commander'),
     pkg = require('../package.json'),
     utilities = require('./utils/utilities.js'),
     VERBOSE = false;
 
 program
-    .version(pkg.version, '-v, --version')
+    .version(pkg.version)
     .usage('<package.json> [options]')
     .option('-p, --version-prefix <prefix>', 'Optional package version prefix to prepend', '^')
     .option('-s, --indent-spaces <num_spaces>', 'Number of spaces of indentation for package.json', 4)
     .option('-i, --version-increment [level]', 'Package.json version increment level {major|minor|patch}', 'patch')
-    .option('-V, --verbose', 'Verbose mode', false)
+    .option('-v, --verbose', 'Verbose mode', false)
     .option('-U, --upgrade-level [level]', 'Dependency version upgrade level {major|minor|patch}', 'major')
     .option('-D, --dry-run', 'Show the upgrades that would be performed instead of upgrading', false)
     .on('--help', utilities.printExamples)
     .parse(process.argv);
+
+const options = program.opts();
 
 function validateOptions() {
     if (!(program.args && program.args.length)) {
         return invalidOption('No package path provided.');
     }
 
+    console.log('ARGS', options);
+
     const validIncrementLevels = ['major', 'minor', 'patch'];
-    if (!validIncrementLevels.includes(program.versionIncrement)) {
+    if (!validIncrementLevels.includes(options.versionIncrement)) {
         return invalidOption('Invalid version increment level provided. Valid levels are: ' + validIncrementLevels);
     }
-    if (!validIncrementLevels.includes(program.upgradeLevel)) {
+    if (!validIncrementLevels.includes(options.upgradeLevel)) {
         return invalidOption('Invalid dependency version upgrade increment level provided. Valid levels are: ' + validIncrementLevels);
     }
 
-    VERBOSE = program.verbose;
+    VERBOSE = options.verbose;
 
     return Promise.resolve({
         packagePath: program.args && program.args.length && program.args[0],
         package: {}, // populated later once the package is read and parsed
         options: {
-            indentationSpaces: parseInt(program.indentSpaces, 10),
-            versionPrefix: program.versionPrefix,
-            versionIncrement: program.versionIncrement,
-            dependencyUpgradeLevel: program.upgradeLevel,
-            dryRun: program.dryRun
+            indentationSpaces: parseInt(options.indentSpaces, 10),
+            versionPrefix: options.versionPrefix,
+            versionIncrement: options.versionIncrement,
+            dependencyUpgradeLevel: options.upgradeLevel,
+            dryRun: options.dryRun
         }
     });
 }
